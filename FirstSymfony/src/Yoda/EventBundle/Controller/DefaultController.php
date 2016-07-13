76944536813d10ebdc5988694d52a8ab880dfc04
Adrian Ispas <adi.ispas95@gmail.com>
2016-07-13 14:30:31 +0300

    Finish Video 7 - 8

--- FirstSymfony/src/Yoda/EventBundle/Controller/DefaultController.php
+++ FirstSymfony/src/Yoda/EventBundle/Controller/DefaultController.php
@@ -10,12 +10,12 @@ class DefaultController extends Controller
     {
 
         //$em = $this->container->get('doctrine')->getManager();
-//        $em = $this->getDoctrine()->getManager();
-//        $repo = $em->getRepository('EventBundle:Event');
-//
-//        $event = $repo->findOneBy(array('name' => 'Darth\'s surprise birthday party'));
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('EventBundle:Event');
 
-        return $this->render('EventBundle:Default:index.html.twig', array('name' => $firstName, 'count' => $count));
+        $event = $repo->findOneBy(array('name' => 'Darth\'s surprise birthday party'));
+
+        return $this->render('EventBundle:Default:index.html.twig', array('name' => $firstName, 'count' => $count, 'event' => $event));
 
         //return new Response($content);
 
--- FirstSymfony/web/play.php
+++ FirstSymfony/web/play.php
@@ -16,22 +16,22 @@ $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
 
-//$kernel->boot();
-//$container = $kernel->getContainer();
-////$container->enterScope('request');
-//$container->set('request',$request);
-//
-////$templating = $container->get('templating');
-////echo $templating->render('EventBundle:Default:index.html.twig', array('name' => 'Vader', 'count' => 3));
-//
-//use Yoda\EventBundle\Entity\Event;
-//
-//$event = new Event();
-//$event->setName('Darth\'s surprise birthday party');
-//$event->setLocation('Deathstar');
-//$event->setTime(new \DateTime('tomorrow noon'));
-//$event->setDetails('Ha! Darth HATES surprises!');
-//
-//$em = $container->get('doctrine')->getManager();
-//$em->persist($event);
-//$em->flush();
+$kernel->boot();
+$container = $kernel->getContainer();
+//$container->enterScope('request');
+$container->set('request',$request);
+
+//$templating = $container->get('templating');
+//echo $templating->render('EventBundle:Default:index.html.twig', array('name' => 'Vader', 'count' => 3));
+
+use Yoda\EventBundle\Entity\Event;
+
+$event = new Event();
+$event->setName('Darth\'s surprise birthday party');
+$event->setLocation('Deathstar');
+$event->setTime(new \DateTime('tomorrow noon'));
+$event->setDetails('Ha! Darth HATES surprises!');
+
+$em = $container->get('doctrine')->getManager();
+$em->persist($event);
+$em->flush();
