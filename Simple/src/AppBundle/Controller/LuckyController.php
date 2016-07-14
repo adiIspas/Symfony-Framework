9c4f3c192fece52bf12ef790ed628992e061d3a2
Adrian Ispas <adi.ispas95@gmail.com>
2016-07-14 11:40:28 +0300

    Finish L4

---
+++ Simple/src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adrian.ispas
+ * Date: 7/14/2016
+ * Time: 10:04 AM
+ */
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/{limit}")
+     */
+    public function numberAction($limit)
+    {
+        $number = rand(1,$limit);
+
+        return $this->render('number/show.html.twig', ['number' => $number]);
+    }
+
+    /**
+     * @Route("/api/lucky/{limit}")
+     */
+    public function apiNumberAction($limit)
+    {
+        $data = array(
+            'lucky_number' => rand(1, $limit),
+        );
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/more/lucky/{limit}")
+     * @param $limit
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function moreNumberAction($limit)
+    {
+        $count = rand(5, $limit);
+
+        $numbers[] = array();
+        for ($i = 0; $i < $count; $i++) {
+            $numbers[] = rand(1, $limit);
+        }
+
+        return $this->render('number/show.html.twig', ['number' => 0, 'numbers' => $numbers]);
+    }
+}
